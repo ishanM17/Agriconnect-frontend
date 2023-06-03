57d1ddfab7c6bf3ec51809314f29ca9db70d28f1
ishanm17 <mulchandaniishan@gmail.com>
2023-06-04 05:11:54 +0530

    fix: charts

--- scripts/farmer.js
+++ scripts/farmer.js
@@ -1,3 +1,5 @@
+let totSales = 0;
+
 fetch("http://localhost:3000/sale/getFarmerPost/" + sessionStorage.getItem('userId'), {
   method: "GET",
   headers: {
@@ -8,9 +10,43 @@ fetch("http://localhost:3000/sale/getFarmerPost/" + sessionStorage.getItem('user
   .then((response) => response.json())
   .then((json) => {
     console.log(json);
-    document.querySelector(".Produce").innerHTML = json.quantity + " Kgs";
-    document.querySelector(".Sale").innerHTML = json.price + " ₹";
+    let totProd = 0;
+    let totRev = 0;
+    let soldQuant = 0;
+    json.forEach((ele) => {
+      totProd += ele.quantity;
+      if (ele.buyer !== undefined) {
+        totRev += ele.price;
+        soldQuant += ele.quantity;
+      }
+    });
+    totSales = soldQuant;
+    document.querySelector(".Produce").innerHTML = totProd + " Kgs";
+    document.querySelector(".Sale").innerHTML = totRev + " ₹";
+    const ctx = document.getElementById("myChart");
+
+      var myChart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: ["Sold", "Total Produce"],
+          datasets: [
+            {
+              label: "quantity in kg",
+              data: [soldQuant, totProd],
+              borderWidth: 1,
+              backgroundColor: ["rgb(0,128,0)", "rgb(197,236,197)"],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+        },
+      }); 
+    
+
   });
+
+
 
 fetch("http://localhost:3000/rent/getBorrowerPost/" + sessionStorage.getItem('userId'), {
   method: "GET",
@@ -22,6 +58,35 @@ fetch("http://localhost:3000/rent/getBorrowerPost/" + sessionStorage.getItem('us
   .then((response) => response.json())
   .then((json) => {
     console.log(json);
-    document.querySelector(".Renting").innerHTML = json.budget + " ₹";
-    
+    let totRent = 0;
+    json.forEach((ele) => {
+      if (ele.lender !== undefined) {
+        totRent += ele.budget;
+      }
+    })
+
+    const ctx = document.getElementById("myChart");
+    const renting = document.getElementById("Revenue_renting");
+
+    var myChart = new Chart(renting, {
+      type: "doughnut",
+      data: {
+        labels: ["Rent", "Sale"],
+        datasets: [
+          {
+            label: "Rupees",
+            data: [totRent, totSales],
+            borderWidth: 1,
+            backgroundColor: ["rgb(99,250,144)", "rgb(128,220,105)"],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    });
+
+
+    document.querySelector(".Renting").innerHTML = totRent + " ₹";
+
   });
--- scripts/my_chart.js
+++ scripts/my_chart.js
@@ -20,38 +20,38 @@ var myChart = new Chart(produce, {
   },
 });
 
-var myChart = new Chart(ctx, {
-  type: "doughnut",
-  data: {
-    labels: ["Sold", "Total Purchase"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [2, 2],
-        borderWidth: 1,
-        backgroundColor: ["rgb(0,128,0)", "rgb(235, 254, 235)"],
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-  },
-});
+// var myChart = new Chart(ctx, {
+//   type: "doughnut",
+//   data: {
+//     labels: ["Sold", "Total Produce"],
+//     datasets: [
+//       {
+//         label: "# of Votes",
+//         data: [2, 2],
+//         borderWidth: 1,
+//         backgroundColor: ["rgb(0,128,0)", "rgb(235, 254, 235)"],
+//       },
+//     ],
+//   },
+//   options: {
+//     responsive: true,
+//   },
+// });
 
-var myChart = new Chart(renting, {
-  type: "doughnut",
-  data: {
-    labels: ["Rent", "Sell"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [2, 3],
-        borderWidth: 1,
-        backgroundColor: ["rgb(99,250,144)", "rgb(128,220,105)"],
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-  },
-});
+// var myChart = new Chart(renting, {
+//   type: "doughnut",
+//   data: {
+//     labels: ["Rent", "Sell"],
+//     datasets: [
+//       {
+//         label: "# of Votes",
+//         data: [2, 3],
+//         borderWidth: 1,
+//         backgroundColor: ["rgb(99,250,144)", "rgb(128,220,105)"],
+//       },
+//     ],
+//   },
+//   options: {
+//     responsive: true,
+//   },
+// });
